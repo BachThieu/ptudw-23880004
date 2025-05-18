5efa4a46e2cd1ebef74ca3d75a361cfbf7c31863
BachThieu <thieuquangbach1997@gmail.com>
2025-05-18 10:10:33 +0700

    250518

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const expressHandlebars = require('express-handlebars');
+const {createStarList} = require('./controller/handlebarsHelper');
+const {createPagination} = require('express-handlebars-paginate');
+const session = require('express-session');
 
 // cau hinh public static folder
 app.use(express.static(__dirname +'/public'));
@@ -14,18 +17,57 @@ app.engine('hbs',expressHandlebars.engine({
     partialsDir: __dirname + '/views/partials',
     extname: 'hbs',
     defaultLayout: 'layout',
+    runtimeOptions: {
+        allowProtoPropertiesByDefault: true,
+    },
+    helpers: {
+        createStarList,
+        createPagination
+    }   
 }));
 app.set('view engine', 'hbs');
 
-// routes
-app.get('/', (req, res) => {
-   res.render('index');
+// Cau hinh doc du lieu post tu body
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// Cau hinh su dung session
+app.use(session({
+    secret: 'S3cret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        httpOnly: true,
+        maxAge: 20* 60 *1000 // 20ph
+    }
+}));
+
+// mideleware khoi tao gioi hang
+app.use((req, res, next) =>{
+    let Cart = require('./controller/cart');
+    req.session.cart = new Cart(req.session.cart ? req.session.cart: {});
+    res.locals.quantity = req.session.cart.quantity;
+
+    next();
 });
 
-app.get('/:page', (req, res) => {
-    res.render(req.params.page);
-    });
-    
+
+// routes
+app.use('/', require('./routes/indexRouter'));
+app.use('/products', require('./routes/productsRouter'));
+app.use('/users', require('./routes/usersRouter'));
+
+
+
+app.use((req, res, next) => {
+    res.status(404).render('error', {'message': 'File not found'});
+});
+
+app.use((error, req, res, next) => {
+    console.error(error);
+    res.status(500).render('error', {'message': 'Internal server error'});
+});
+
 // khoi dong web server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
---
+++ routes/productsRouter.js
@@ -0,0 +1,19 @@
+'use strict';
+const express = require('express');
+let router = express.Router();
+let controller = require('../controller/productsController');
+let cartController = require('../controller/cartController');
+router.get('/',controller.getData, controller.show);
+
+
+router.get('/cart', cartController.show);
+
+router.get('/:id',controller.getData, controller.showDetails);
+
+router.post('/cart', cartController.add);
+
+router.put('/cart', cartController.update);
+router.delete('/cart', cartController.remove);
+router.delete('/cart/all', cartController.clear);
+
+module.exports = router;
